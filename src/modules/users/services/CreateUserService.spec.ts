4d532f9cc1e41e995c6aad5df4341ad9dcdc6a5f
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-04-11 17:27:35 -0300

    update tests

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,17 @@
 import AppError from '@shared/errors/AppError'
 import FakeUsersRepository from '@modules/users/tests/fakes/FakeUsersRepository'
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProviders'
 import CreateUserService from './CreateUserService'
 
 describe('CreateUsers', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
-    const createUserService = new CreateUserService(fakeUsersRepository)
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
 
     const user = await createUserService.execute({
       name: 'Felipe Rocha Oliveira',
@@ -18,7 +24,12 @@ describe('CreateUsers', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
-    const createUserService = new CreateUserService(fakeUsersRepository)
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    )
 
     await createUserService.execute({
       name: 'Felipe Rocha Oliveira',
