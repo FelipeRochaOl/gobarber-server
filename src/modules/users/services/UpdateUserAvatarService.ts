cabf1fbdb88c65433c05d4f5b5b3db5b568c4778
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-04-11 19:32:36 -0300

    add coverage 100% tests in gobarber

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,12 +1,10 @@
-import path from 'path'
-import fs from 'fs'
+import { inject, injectable } from 'tsyringe'
 
 import User from '@modules/users/infra/typeorm/entities/User'
 
-import uploadConfig from '@config/upload'
 import AppError from '@shared/errors/AppError'
-import { inject, injectable } from 'tsyringe'
-import IUsersRepository from '../interfaces/IUsersRepository'
+import IUsersRepository from '@modules/users/interfaces/IUsersRepository'
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 
 interface IRequest {
   user_id: string
@@ -17,8 +15,14 @@ interface IRequest {
 class UpdateUserAvatarService {
   private usersRepository: IUsersRepository
 
-  constructor(@inject('UsersRepository') usersRepository: IUsersRepository) {
+  private storageProvider: IStorageProvider
+
+  constructor(
+    @inject('UsersRepository') usersRepository: IUsersRepository,
+    @inject('StorageProvider') storageProvider: IStorageProvider,
+  ) {
     this.usersRepository = usersRepository
+    this.storageProvider = storageProvider
   }
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -29,15 +33,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await this.storageProvider.deleteFile(user.avatar)
     }
 
-    user.avatar = avatarFilename
+    const filename = await this.storageProvider.saveFile(avatarFilename)
+
+    user.avatar = filename
 
     await this.usersRepository.update(user)
 
